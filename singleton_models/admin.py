b6bef41b8eae84a42b31414424b3d4fd1d999fe6
AK1N Nemesis Fixx <joewillrich@gmail.com>
2018-03-23 13:06:49 +0000

    apply a fix, to intro compat with Django==1.11.11

--- singleton_models/admin.py
+++ singleton_models/admin.py
@@ -21,16 +21,16 @@ class SingletonModelAdmin(admin.ModelAdmin):
         try:
             from django.conf.urls.defaults import patterns, url
         except ImportError:
-            from django.conf.urls import patterns, url
+            from django.conf.urls import url
 
         def wrap(view):
             def wrapper(*args, **kwargs):
                 return self.admin_site.admin_view(view)(*args, **kwargs)
             return update_wrapper(wrapper, view)
 
-        info = self.model._meta.app_label, self.model._meta.module_name
+        info = self.model._meta.app_label, self.model._meta.model_name
 
-        urlpatterns = patterns('',
+        urlpatterns = [
             url(r'^history/$',
                 wrap(self.history_view),
                 {'object_id': '1'},
@@ -39,7 +39,7 @@ class SingletonModelAdmin(admin.ModelAdmin):
                 wrap(self.change_view),
                 {'object_id': '1'},
                 name='%s_%s_changelist' % info),
-        )
+            ]
         return urlpatterns
 
     def response_change(self, request, obj):
